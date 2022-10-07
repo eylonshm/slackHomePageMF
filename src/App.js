05af7a089539567693d942c74df85e96cfc25a9b
eylonshm <eylonshm@gmail.com>
2022-10-07 19:35:01 +0300

    Prepair app to be merge-ready to container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
-const helloWorld = () => <div>Hello World!</div>
-const helloReact = () => <div>Hello React!</div>
+const HelloWorld = () => <div>Hello World!</div>
+const HelloReact = () => <div>Hello React!</div>
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='react' element={<>React route</>} />
-          <Route path='/' element={<>Root route</>} />
+          <Route path='react' element={<HelloReact />} />
+          <Route path='/' element={<HelloWorld />} />
         </Routes>
+        <Link to='/react'>Say hello to React!</Link>
+        <br />
+        <Link to='/'>Say hello to the World!</Link>
       </BrowserRouter>
     </div>
   )
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { createBrowserHistory } from 'history'
 
 const mount = (el) => {
+  const history = createBrowserHistory()
+
   const root = createRoot(el)
   root.render(<App />)
-  //   return {}
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location
+      if (pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    },
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
